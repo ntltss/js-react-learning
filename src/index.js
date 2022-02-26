27d34c336d310a764d0fd5c0c979710363b145c4
HiroyukiWindows <nevertoolatetostartsomething@gmail.com>
2022-02-26 18:20:14 +0900

    test main

--- src/index.js
+++ src/index.js
@@ -142,6 +142,7 @@ reactDom.render(
 
 //confilict_from_windows
 //test_branch_feature/TRPF-3
+//test_main
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
