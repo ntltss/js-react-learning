293affd7fe6dec098755d56a22c543ff410ea3ff
unknown <hoonuma@DESKTOP-4HDQO81.solxyz.co.jp>
2022-02-26 16:15:28 +0900

    test from Windwos

--- src/index.js
+++ src/index.js
@@ -137,6 +137,8 @@ reactDom.render(
   document.getElementById('root14')
 );
 
+//test_from_Windows
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
